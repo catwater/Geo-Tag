3858e2c7fe73ad8d4dda1cc4d5c92b4ec0c40c37
catwater <catwater37@gmail.com>
2016-03-30 15:02:44 -0700

    Initial commit

---
+++ app/src/main/java/com/geotag/tagx5/geotag/MainActivity.java
@@ -0,0 +1,13 @@
+package com.geotag.tagx5.geotag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
