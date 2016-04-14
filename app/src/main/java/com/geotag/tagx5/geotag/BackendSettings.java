98949bab935fbc702c14b235b56dc4bafde12d6e
kriskriskris10 <kristenk210@gmail.com>
2016-04-14 08:57:14 -0700

    blah

---
+++ app/src/main/java/com/geotag/tagx5/geotag/BackendSettings.java
@@ -0,0 +1,13 @@
+package com.geotag.tagx5.geotag;
+
+
+
+/**
+ * Created by csastudent2015 on 2/29/16.
+ */
+public class BackendSettings {
+
+    public static final String APPLICATION_ID = "";
+    public static final String ANDROID_SECRET_KEY = "";
+    public static final String VERSION = "v1";
+}
