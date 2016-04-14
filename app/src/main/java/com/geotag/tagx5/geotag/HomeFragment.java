f858e1606d64949fbc7211af755612567841618f
kriskriskris10 <kristenk210@gmail.com>
2016-04-14 09:41:45 -0700

    blah

---
+++ app/src/main/java/com/geotag/tagx5/geotag/HomeFragment.java
@@ -0,0 +1,9 @@
+package com.geotag.tagx5.geotag;
+
+import android.app.Fragment;
+
+/**
+ * Created by csastudent2015 on 4/14/16.
+ */
+public class HomeFragment extends Fragment {
+}
--- app/src/main/java/com/geotag/tagx5/geotag/LoginActivity.java
+++ app/src/main/java/com/geotag/tagx5/geotag/LoginActivity.java
@@ -39,7 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         mRegistrationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
                 startActivityForResult(intent, REGISTER_REQUEST_CODE);
 
 
