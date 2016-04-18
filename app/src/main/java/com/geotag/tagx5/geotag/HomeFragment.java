3653759830b0fe0b71fd07356485b7b1991dae92
kriskriskris10 <kristenk210@gmail.com>
2016-04-18 09:24:44 -0700

    blah

--- app/src/main/java/com/geotag/tagx5/geotag/HomeFragment.java
+++ app/src/main/java/com/geotag/tagx5/geotag/HomeFragment.java
@@ -1,9 +1,32 @@
 package com.geotag.tagx5.geotag;
 
-import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by csastudent2015 on 4/14/16.
  */
 public class HomeFragment extends Fragment {
+
+    private TextView mScore;
+    private TextView mLivesRemaining;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.home_fragment, container, false);
+        mScore = (TextView) rootView.findViewById(R.id.text_score);
+        mLivesRemaining = (TextView) rootView.findViewById(R.id.text_lives_remaining);
+
+
+        Intent i = new Intent();
+        i = getActivity().getIntent();
+
+        return rootView;
+    }
+
+
 }
--- app/src/main/java/com/geotag/tagx5/geotag/LoadingCallback.java
+++ app/src/main/java/com/geotag/tagx5/geotag/LoadingCallback.java
@@ -1,59 +1,59 @@
-//package com.geotag.tagx5.geotag;
-//
-///**
-// * Created by csastudent2015 on 4/14/16.
-// */
-//
-//import android.app.ProgressDialog;
-//import android.content.Context;
-//
-//import com.backendless.async.callback.AsyncCallback;
-//import com.backendless.exceptions.BackendlessFault;
-//
-///**
-// * Created by csastudent2015 on 2/29/16.
-// */
-//public class LoadingCallback<T> implements AsyncCallback<T> {
-//
-//    private Context c;
-//    private ProgressDialog p;
-//
-//    public LoadingCallback(Context c)
-//    {
-//        this.c=c;
-//        p = new ProgressDialog(c);
-//        p.setMessage("LOADING LOADING LOADING");
-//
-//    }
-//    public LoadingCallback(Context c, String message)
-//    {
-//        this.c=c;
-//        p = new ProgressDialog(c);
-//        p.setMessage(message);
-//
-//    }
-//
-//    @Override
-//    public void handleResponse(T response) {
-//        p.dismiss();
-//
-//
-//    }
-//
-//    @Override
-//    public void handleFault(BackendlessFault fault) {
-//        p.dismiss();
-//        DialogHelper.createErrorDialog(c,"Backendless Fault", fault.getMessage(), "close").show();
-//
-//    }
-//
-//    public void showDialog()
-//    {
-//        p.show();
-//    }
-//
-//    public void hideDialog(){
-//        p.hide();
-//    }
-//}
-//
+package com.geotag.tagx5.geotag;
+
+/**
+ * Created by csastudent2015 on 4/14/16.
+ */
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+
+/**
+ * Created by csastudent2015 on 2/29/16.
+ */
+public class LoadingCallback<T> implements AsyncCallback<T> {
+
+    private Context c;
+    private ProgressDialog p;
+
+    public LoadingCallback(Context c)
+    {
+        this.c=c;
+        p = new ProgressDialog(c);
+        p.setMessage("LOADING LOADING LOADING");
+
+    }
+    public LoadingCallback(Context c, String message)
+    {
+        this.c=c;
+        p = new ProgressDialog(c);
+        p.setMessage(message);
+
+    }
+
+    @Override
+    public void handleResponse(T response) {
+        p.dismiss();
+
+
+    }
+
+    @Override
+    public void handleFault(BackendlessFault fault) {
+        p.dismiss();
+        DialogHelper.createErrorDialog(c,"Backendless Fault", fault.getMessage(), "close").show();
+
+    }
+
+    public void showDialog()
+    {
+        p.show();
+    }
+
+    public void hideDialog(){
+        p.hide();
+    }
+}
+
